4106a9e87fd81ffb088591202f0b65e17c9e2949
mitchell <mitchell.kelleher@my.jcu.edu.au>
2019-03-27 10:49:33 +1030

    Initialising file with starting notes.

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,18 @@
+"""
+CP1404 - Practical
+Write a program to:
+- count the occurrences of words in a string.
+- Ask the user for a string
+- print the counts of how many of each word are in the file
+- After you have the program working, make your program do this sorting
+- align the numbers so they are in one nice column (see prac for notes)
+"""
+
+# Prompt for text
+
+# place each word into a dictionary with a count of 1, check if it exists, if so add a count
+
+# print the dictionary (each word and count)
+# add in sort and align them to a column(use a variable that calculates the longest word)
+# hint: print("{:{}} = {}".format(x, y, z))
+
