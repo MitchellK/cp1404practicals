a0937f4147fe714eaa9d659bc6ef04f40105eb4e
MitchellK <mitchell.kelleher@my.jcu.edu.au>
2019-03-28 22:04:43 +1030

    Word Occurences update

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,22 +1,65 @@
 """
 CP1404 - Practical
 Write a program to:
-- count the occurrences of words in a string.
+- Count the occurrences of words in a string.
 - Ask the user for a string
-- print the counts of how many of each word are in the file
+- Print the counts of how many of each word are in the file
 - After you have the program working, make your program do this sorting
-- align the numbers so they are in one nice column (see prac for notes)
+- Align the numbers so they are in one nice column (see prac for notes)
 """
 
-# Prompt for text
-string = input("enter a phrase:")
-# place each word into a dictionary with a count of 1, check if it exists, if so add a count
-new_list = []
-for each in string:
-    new_list.append(each)
-print(new_list)
 
-# print the dictionary (each word and count)
-# add in sort and align them to a column(use a variable that calculates the longest word)
-# hint: print("{:{}} = {}".format(x, y, z))
+def main():
+    # Prompt for text
+    phrase = input("enter a phrase:")
+    new_dictionary = convert_to_dict(phrase)
+    longest_word = get_longest_word(new_dictionary)
 
+    # TODO: need to improve sorting. should use tuples of dict instead
+    # add in sort by key
+    sort_list = []
+    for key in new_dictionary:
+        sort_list.append(key)
+    sort_list.sort()
+
+    # sort by amount
+    second_sort_list = []
+    for k in new_dictionary.values():
+        if k not in second_sort_list:
+            second_sort_list.append(k)
+    second_sort_list.sort(reverse=True)
+
+    # print the dictionary (each word and count)
+    # hint: print("{:{}} = {}".format(x, y, z))
+    print("Below is the dictionary sorted by key:")
+    for i in sort_list:
+        print("{:{}}: {}".format(i, longest_word, new_dictionary[i]))
+
+    print("\n\nBelow is the dictionary sorted by value")
+    for n in second_sort_list:
+        for ke, val in new_dictionary.items():
+            if n == val:
+                print("{:{}}: {}".format(ke, longest_word, val))
+
+
+def convert_to_dict(string):
+    # place each word into a dictionary with a count of 1, check if it exists, if so add a count
+    dictionary = {}
+    for each in string.split():
+        try:
+            dictionary[each] += 1
+        except KeyError:
+            dictionary[each] = 1
+    return dictionary
+
+
+def get_longest_word(dictionary):
+    # align them to a column(use a variable that calculates the longest word)
+    length = 0
+    for word in dictionary:
+        if len(word) > length:
+            length = int(len(word))
+    return length
+
+
+main()
